8a110b132bd2d1ddd4fd96018dc4ecdc0d0bbc64
Jose <josevallino18@gmail.com>
2025-08-22 20:15:54 +0200

    modules

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract FundMeTestIntegration is Test {
+
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        // Give funds to the FundFundMe contract so it can fund the FundMe contract
+        vm.deal(address(fundFundMe), 1 ether);
+        fundFundMe.fundFundMe(address(fundMe));
+
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address (fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
